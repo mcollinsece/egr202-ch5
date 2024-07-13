659f4d0074211976654668aff374365e9978377d
Matthew <matthewcollinsece@gmail.com>
2024-07-13 15:14:01 -0400

    Updates

--- lab5-6.cpp
+++ lab5-6.cpp
@@ -9,6 +9,25 @@ using namespace std;
 
 int main()
 {
+	char code = ' ';
+	double purchase = 0.0;
+	double serviceChg = 0.0;
 
+	//enter input data
+	cout << "Enter code: ";
+	cin >> code;
+	cout << "Purchase price: ";
+	cin >> purchase;
+
+	//calculate service charge
+	serviceChg = purchase * 0.01;
+	//add $5 to code 2 purchases
+	if (code = '2')
+		serviceChg = serviceChg + 5;
+	//end if
+
+	//display service charge
+	cout << fixed << setprecision(2);
+	cout << "Service charge: " << serviceChg << endl;
 	return 0;
 }   //end of main function
